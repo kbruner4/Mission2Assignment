99c9a3eb2c1a46d93e51ebf8aff16ab17aee2f63
kbruner4 <90485716+kbruner4@users.noreply.github.com>
2022-01-25 18:33:01 -0700

    glhjkh

---
+++ Mission2Assignment/Controllers/BlahController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission2Assignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    [HttpGet]
+    public IActionResult Calculator ()
+        {
+            return View();
+        }
+    [HttpPost]
+    public IActionResult Calculator(CalculatorModel model)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Mission2Assignment/Models/CalculatorModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment.Models
+{
+    public class CalculatorModel
+    {
+        [Required]
+        public int Assignment { get; set; }
+        [Required]
+        public int GroupProject { get; set; }
+        [Required]
+        public int Quizzes { get; set; }
+        [Required]
+        public int Exams { get; set; }
+        [Required]
+        public int Intex { get; set; }
+    }
+}
